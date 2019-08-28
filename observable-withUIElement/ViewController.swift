a90547db3fef9e479a5be73f18586f19ed208e33
khaled elsedek <kh.elsedek@gmail.com>
2019-08-28 11:45:05 +0200

    Initial Commit

---
+++ observable-withUIElement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  observable-withUIElement
+//
+//  Created by khaled elsedek on 8/28/19.
+//  Copyright © 2019 khaled elsedek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
